715a0483dc41488efc428f4158eeb25201916719
Bhoumik Miskin <bhoumik.miskin@s.amity.edu>
2025-05-06 10:29:56 +0530

    Update hello.cpp
    changed the  printf statements and the variables

--- hello.cpp
+++ hello.cpp
@@ -2,17 +2,17 @@
 
 int main()
 { 
-	int num1, num2, sum ;
+	int a, b, s ;
 
-	printf("Enter the first number:");
-	scanf("%d" , &num1);
+	printf("Enter the number:");
+	scanf("%d" , &a);
 
-	printf("Enter the second number:");
-	scanf("%d" , &num2);
+	printf("Enter the number:");
+	scanf("%d" , &b);
 
-        sum= num1 + num2;
+        s= a+b;
 
-	printf("The sum of %d and %d and %d is: %d |\n", num1, num2, sum);
+	printf("The sum of %d and %d and %d is: %d |\n", a, b, s);
 
 	return 0;
 }
